4d9226dc6c243a81278e1bd9aa949aae1af5efab
MuhamadJalal <muhamadgalal4@gmail.com>
2023-08-10 09:23:10 -0700

    Add 4-puts.c task!

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
-
+/*
+start to use puts to print var with new line
+*/
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle");
+puts("\"Programming is like building a multilingual puzzle\"");
 return (0);
 }
