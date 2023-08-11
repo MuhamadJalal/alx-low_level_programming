1a5f7c85cd17b8f37fcd3ac45e77642eabebf54b
MuhamadJalal <muhamadgalal4@gmail.com>
2023-08-11 16:44:01 -0700

    Add 102-print_comb5.c file

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int c, i;
+for (c = 0; c < 100; c++)
+{
+for (i = c; i < 100; i++)
+{
+putchar(48 + c);
+putchar(48 + i);
+if (c != i)
+if (c != 98)
+putchar(',');
+if (c != i)
+if (c != 98)
+putchar(' ');
+}
+}
+putchar('\n');
+return (0);
+}
