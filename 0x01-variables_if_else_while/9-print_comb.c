885c62b8c420f3db1adf30e611c1a95b530951c6
MuhamadJalal <muhamadgalal4@gmail.com>
2023-08-11 16:01:51 -0700

    Add 9-print_comb.c file

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int c;
+char s;
+for (c = 0; c < 10; c++)
+{
+putchar(c);
+putchar('\, ');
+}
+putchar('\n');
+return (0);
+}
