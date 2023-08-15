062761124bf1ca04a359c8bb207b33b76c38634a
MuhamadJalal <muhamadgalal4@gmail.com>
2023-08-15 15:34:44 -0700

    Add 10-add.c file

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* add - entry point
+*
+* Description: This function that adds two integers and returns the result.
+*
+* @a: number will be added to another number b
+* @b: number will be added to another number a
+* Return: int
+*/
+int add(int a, int b)
+{
+return (a + b);
+}
