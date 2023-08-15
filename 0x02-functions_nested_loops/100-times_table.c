0b80537112be0638b9bab7ef7494ea46f30e13e5
MuhamadJalal <muhamadgalal4@gmail.com>
2023-08-15 16:25:49 -0700

    Add 100-times_table.c file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,11 +11,7 @@
 void print_times_table(int n)
 {
 int i, j, k;
-if(n == 0)
-{
-_putchar('0');
-}
-else if (n > 0 && n < 15)
+if (n >= 0 && n <= 15)
 {
 for (i = 0; i <= n; i++)
 {
@@ -26,20 +22,28 @@ if (j == 0)
 {
 _putchar(k + '0');
 }
-if (k < n && j != 0)
+else if (k < 10 && j != 0)
 {
 _putchar(',');
 _putchar(' ');
 _putchar(' ');
 _putchar(' ');
+_putchar(k + '0');
+}
+else if (k >= 10 && k < 100)
+{
+_putchar(',');
+_putchar(' ');
+_putchar(' ');
+_putchar((k / 10) + '0');
 _putchar((k % 10) + '0');
 }
-else if (k >= n)
+else if (k >= 100)
 {
 _putchar(',');
 _putchar(' ');
-_putchar(' ');
-_putchar(' ');
+_putchar((k / 100) + '0');
+_putchar(((k / 10) % 10) + '0');
 _putchar((k % 10) + '0');
 }
 }
