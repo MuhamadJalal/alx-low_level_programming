231f1399b60f672dd4231f4ec454b7661fe611c3
MuhamadJalal <muhamadgalal4@gmail.com>
2023-08-15 14:44:35 -0700

    Add 7-print_last_digit.c file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,14 +11,13 @@
 int print_sign(int n)
 {
 int i;
-if (n > 0)
+if (n < 0)
 {
+n = -n;
+}
 i = n % 10;
-}
-else if (n < 0)
-{
-i = (-n) % 10;
-}
+if (i < 0)
+i = -i;
 _putchar(i + '0');
 return (i);
 }
