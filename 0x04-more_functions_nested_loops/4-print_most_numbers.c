021ed10bfb38dfca6a72198a444cffde7fc14af6
MuhamadJalal <muhamadgalal4@gmail.com>
2023-08-17 16:38:03 -0700

    update and fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+11;rgb:0000/0000/0000
 /**
  * print_most_numbers - prints the numbers from 1 to 100
  * but for multiples of three prints Fizz instead of the number
@@ -13,7 +13,7 @@ int i = 0;
 while (i < 10)
 {
 
-if (i != 2 && i != 4)
+if (!(i == 2 || i == 4))
 _putchar(i % 10);
 
 i++;
