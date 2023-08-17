00cbad8bf17670ddc0eae72259e0d43f8eb9bfc4
MuhamadJalal <muhamadgalal4@gmail.com>
2023-08-16 17:27:22 -0700

    add 104-fibonacci.c file

---
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+unsigned long start = 0, end = 1, sum = 0;
+for (i = 0; i < 50; i++)
+{
+sum = start + end;
+printf("%lu", sum);
+start = end;
+end = sum;
+if (i == 49)
+printf("\n");
+else
+printf(", ");
+}
+return (0);
+}
