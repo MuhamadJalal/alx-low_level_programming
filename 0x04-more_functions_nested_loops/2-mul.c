1f4304616f880312c426e2cd5475cbd9aa2b49f0
MuhamadJalal <muhamadgalal4@gmail.com>
2023-08-17 03:16:38 -0700

    Update 2-mul.c file

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -2,12 +2,12 @@
 
 
 /**
-* _mul - check the code.
+* mul - check the code.
 * @c: is the char to test
 * @x: another num to multible with.
 * Return: 0 || 1.
 */
-int _mul(int c, int x)
+int mul(int c, int x)
 {
 return (c * x);
 }
