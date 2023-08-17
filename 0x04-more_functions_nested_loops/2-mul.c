f3cc08fb86bbdd7ddeec00229d44680783779c54
MuhamadJalal <muhamadgalal4@gmail.com>
2023-08-17 16:08:03 -0700

    Add 2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+* mul - check the code.
+*
+* @c: is the char to test
+* @x: another num to multible with.
+*
+* Return: int.
+*/
+int mul(int c, int x)
+{
+return (c * x);
+}
