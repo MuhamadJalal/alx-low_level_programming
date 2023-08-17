26bff034a53fe687df6f0a9795ee74dba544eada
MuhamadJalal <muhamadgalal4@gmail.com>
2023-08-17 10:51:13 -0700

    Update 3-print_numbers.c file

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -3,12 +3,12 @@
 
 /**
 * print_numbers - check the code.
-* Return: 0 || 1.
+* Return: void.
 */
 void print_numbers(void)
 {
 int i = 0;
-while(i < 10)
+while (i < 10)
 {
 _putchar('0' + i);
 i++;
