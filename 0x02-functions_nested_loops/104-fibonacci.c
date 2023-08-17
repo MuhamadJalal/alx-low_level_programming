a00a7b8bd30787e92e4311fee2b25f3536a817fb
MuhamadJalal <muhamadgalal4@gmail.com>
2023-08-16 18:26:39 -0700

    add 104-fibonacci.c file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,20 +7,37 @@
 int main(void)
 {
 int i;
-unsigned long start = 0, end = 1, sum = 0;
-for (i = 0; i < 98; i++)
+unsigned long start = 0, end = 1, sum;
+unsigned long startPart1, startPart2, endPart1, endPart2;
+unsigned long firstPart, secondPart;
+for (i = 0; i < 92; i++)
 {
 sum = start + end;
-if (i < 93)
-printf("%lu", sum);
-else
-printf("%lu", sum + 18446744100000000000);
+printf("%lu, ", sum);
 start = end;
 end = sum;
-if (i == 97)
-printf("\n");
-else
-printf(", ");
 }
+startPart1 = start / 10000000000;
+endPart1 = end / 10000000000;
+startPart2 = start % 10000000000;
+endPart2 = end % 10000000000;
+for (i = 93; i < 99; i++)
+{
+firstPart = startPart1 + endPart1;
+secondPart = startPart2 + endPart2;
+if (startPart2 + endPart2 > 9999999999)
+{
+firstPart += 1;
+secondPart %= 10000000000;
+}
+printf("%lu%lu", firstPart, secondPart);
+if (i != 98)
+printf(", ");
+startPart1 = endPart1;
+startPart2 = endPart2;
+endPart1 = firstPart;
+endPart2 = secondPart;
+}
+printf("\n");
 return (0);
 }
