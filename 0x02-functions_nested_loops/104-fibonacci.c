6c7291705537f65f4f96957cb7941b73cae42cfa
MuhamadJalal <muhamadgalal4@gmail.com>
2023-08-16 17:55:13 -0700

    add 104-fibonacci.c file

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -11,7 +11,10 @@ unsigned long start = 0, end = 1, sum = 0;
 for (i = 0; i < 98; i++)
 {
 sum = start + end;
+if(i < 93)
 printf("%lu", sum);
+else
+printf("%lu", sum + 1000); 
 start = end;
 end = sum;
 if (i == 97)
