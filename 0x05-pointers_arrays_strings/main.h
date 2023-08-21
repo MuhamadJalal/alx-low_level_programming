2f7ab3e5655e97cd5370e2d19a67395604437a46
MuhamadJalal <muhamadgalal4@gmail.com>
2023-08-21 15:33:34 -0700

    Add 5-rev_string.c file

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
-#define MAIN_H
+11;rgb:0000/0000/0000#define MAIN_H
 
 int _putchar(char c);
 void reset_to_98(int *n);
@@ -7,5 +7,6 @@ void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 void print_rev(char *s);
+void rev_string(char *s);
 
 #endif
