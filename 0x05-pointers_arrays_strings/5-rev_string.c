515acef0f5a1185fd306ade15ecf0a10f2dee58f
MuhamadJalal <muhamadgalal4@gmail.com>
2023-08-21 16:47:59 -0700

    Add 5-rev_string.c file

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,22 +9,27 @@ void rev_string(char *s)
 {
 	int i = 0, l, o;
 
-	for (o = 0; s[o] != '\0'; o++)
-	{
-		_putchar(s[o]);
-	}
-
-	_putchar('\n');
-
 	while (s[i] != '\0')
 	{
 		++i;
 	}
 
+	for (o = 0; s[o] != '\0'; o++)
+	{
+		_putchar(s[o]);
+		if (o == (i - 1))
+		{
+		_putchar('\n');
+		}
+	}
+
 	for (l = i - 1; l >= 0; l--)
 	{
 		_putchar(s[l]);
+		if (l == 0)
+		{
+			_putchar('\n');
+		}
 	}
 
-	_putchar('\n');
 }
