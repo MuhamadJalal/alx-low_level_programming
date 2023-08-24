51cc2ea9d998b3299eebf20253c4b734b7129b3d
MuhamadJalal <muhamadgalal4@gmail.com>
2023-08-23 18:54:38 -0700

    Add 0-strcat.c file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,16 +8,16 @@
 */
 char *_strcat(char *dest, char *src)
 {
-int dest_len = 0, src_len = 0,
+	int dest_len = 0, src_len = 0;
 
-while (*(dest + dest_len) != '\0')
-dest_len++;
-while (*(src + src_len) != '\0' && dest_len < 97)
-{
-*(dest + dest_len) = *(src + src_len);
-src_len++;
-dest_len++;
-}
-*(dest + dest_len) = '\0';
-return (dest);
+	while (*(dest + dest_len) != '\0')
+		dest_len++;
+	while (*(src + src_len) != '\0' && dest_len < 97)
+	{
+		*(dest + dest_len) = *(src + src_len);
+		src_len++;
+		dest_len++;
+	}
+	*(dest + dest_len) = '\0';
+	return (dest);
 }
