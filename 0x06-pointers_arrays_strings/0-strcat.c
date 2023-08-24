8a513d8fb7a74b805a02ea1f3160fc0336abab7a
Muhamad Jalal <33559652+MuhamadJalal@users.noreply.github.com>
2023-08-24 04:48:05 +0300

    Add 0-strcat.c file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /*
-* _strcat concatenate src to dest string.
+* _strcat - concatenate src to dest string.
 * @dest: the string that will be appended to.
 * @src: the string needs to add to dest
 * Return: char
