c8c8b567433abce0f0ec22f60239ba8f8a9a0be0
MuhamadJalal <muhamadgalal4@gmail.com>
2023-08-24 11:58:44 -0700

    update 1-strncat.c file

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,8 +2,8 @@
 
 /**
 * _strcat - concatenate src to dest string.
-* @dest: the string that will be appended to.
-* @src: the string needs to add to dest
+* @dest: pointer points to the string that will be appended to.
+* @src: pointer points to the string needs to add to dest
 * @n: the size of src string.
 * Return: char
 */
@@ -12,7 +12,9 @@ char *_strncat(char *dest, char *src, int n)
 	int dest_len = 0, src_len = 0;
 
 	while (*(dest + dest_len) != '\0')
+	{
 		dest_len++;
+	}
 	while (*(src + src_len) != '\0' && dest_len < 97 && src_len < n)
 	{
 		*(dest + dest_len) = *(src + src_len);
