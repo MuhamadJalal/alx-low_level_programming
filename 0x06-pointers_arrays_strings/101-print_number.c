05fa0750acc841d9a3c090acea82538b2621613c
MuhamadJalal <muhamadgalal4@gmail.com>
2023-08-24 18:42:56 -0700

    Add 101-print_number.c file

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,42 +1,49 @@
-#include "main.h"
-
+#include "holberton.h"
+#include <stdio.h>
 /**
- * print_number - print numbers
- * @n: int parameter
- * Description: this function print numbers
- * Return: Nothing
+ * print_number - Print an integer using only _putchar
+ * @n: integer to print
  */
 
 void print_number(int n)
 {
-	int aux;
+	int power;
+	int neg;
+	int hold;
 
+	neg = 0;
+	power = 1;
+	hold = n;
 	if (n < 0)
 	{
 		_putchar('-');
-		n = n * -1;
+		neg = 1;
 	}
 
-	if (n == 0)
+	while (hold > 9 || hold < -9)
 	{
-		_putchar('0');
-	} else if (n > 0 && n < 100)
+		power *= 10;
+		hold /= 10;
+	}
+
+	while (power > 0)
 	{
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
-	} else if (n >= 100 && n <= 999)
-	{
-		aux = n / 10;
-		_putchar(aux / 10 + '0');
-		_putchar(aux % 10 + '0');
-		_putchar(n % 10 + '0');
-	} else if (n >= 1000 && n <= 9999)
-	{
-		aux = n / 100;
-		n = n % 100;
-		_putchar(aux / 10 + '0');
-		_putchar(aux % 10 + '0');
-		_putchar(n / 10 + '0');
-		_putchar(n % 10 + '0');
+		if (power > 9)
+		{
+			if (!neg)
+				_putchar((n / power % 10) + '0');
+			else
+				_putchar((n / power % 10) * -1 + '0');
+
+			power /= 10;
+		}
+		if (power == 1)
+		{
+			if (neg)
+				_putchar((n % 10) * -1 + '0');
+			else
+				_putchar(n % 10 + '0');
+			power = 0;
+		}
 	}
 }
