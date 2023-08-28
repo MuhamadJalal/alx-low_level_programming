7ae26e340bc50c8a03cf07941af30b7075ee3fe0
Muhamad Jalal <33559652+MuhamadJalal@users.noreply.github.com>
2023-08-28 14:02:26 +0300

    Add 4-strpbrk.c file

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+* _strpbrk - searches a string for any of a set of bytes
+* @s: the address of text to locates the first occurrence of accept
+* @accept: the string to find in
+*
+* Return: char.
+*/
+char *_strpbrk(char *s, char *accept)
+{
+int j = 0, i = 0;
+unsigned char a = "";
+
+while (s[i] != '\0')
+{
+while (accept[j] != '\0')
+{
+if (s[i] == accept[j])
+{
+return (s);
+j++;
+}
+i++;
+j = 0;
+}
+return ('\0');
+}
