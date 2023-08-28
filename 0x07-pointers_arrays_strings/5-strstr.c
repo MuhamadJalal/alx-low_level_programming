6f550a9b9d05e8e4697ec738018d2f559e83772b
Muhamad Jalal <33559652+MuhamadJalal@users.noreply.github.com>
2023-08-28 14:25:34 +0300

    Add 5-strstr.c file

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+
+/**
+* _strstr - finds the first occurrence of the substring
+* The terminating null bytes (\0) are not compared
+*
+* @haystack: finds the first occurrence of the substring needle in it
+* @needle: the substring needle to search for
+*
+* Return: char.
+*/
+char *_strstr(char *haystack, char *needle)
+{
+int j = 0, i = 0;
+
+if (needle == '\0')
+{
+return (haystack);
+}
+while (haystack[i] != '\0')
+{
+if (haystack[i] == needle[0])
+{
+int a = 0;
+int indicator = i;
+
+while (needle[j] != '\0')
+{
+if (haystack[i] == needle[j])
+{
+a++;
+}
+j++;
+}
+
+if (a == indicator)
+{
+return (&haystack[i]);
+}
+}
+i++;
+j = 0;
+}
+return ('\0');
+}
