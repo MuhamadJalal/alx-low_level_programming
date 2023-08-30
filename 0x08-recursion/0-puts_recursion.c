2e81cfd32c40086cc472ed054c14a9572f707961
MuhamadJalal <muhamadgalal4@gmail.com>
2023-08-30 07:57:29 -0700

    Add 0-puts_recursion.c file

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -8,16 +8,14 @@
 */
 void _puts_recursion(char *s)
 {
-	while (*s != '\0')
-	{
-		_putchar(*s);
-
-		_puts_recursion(*(s + 1));
-
-	}
-
 	if (*s == '\0')
 	{
 		_putchar('\n');
+		return (0);
 	}
+
+	_putchar(*s);
+
+	_puts_recursion(*(s + 1));
+
 }
