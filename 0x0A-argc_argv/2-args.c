e84871d76707a4d01be341348e6d0cd37cd708c1
MuhamadJalal <muhamadgalal4@gmail.com>
2023-09-01 11:50:07 -0700

    create 2-args.c file

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+* main - prints its name, followed by a new line.
+*
+* @argc: number of command line arguments.
+* @argv: array that contains the program command line arguments.
+*
+* Return: 0 when success.
+*/
+
+int main(int argc, char *argv[])
+{
+	int i = 0;
+
+	for (; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
