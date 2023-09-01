afa1f110df6d7bb4265112339e0e4f28596e7ed1
MuhamadJalal <muhamadgalal4@gmail.com>
2023-09-01 11:58:08 -0700

    create 3-mul.c file

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,13 +11,13 @@
 
 int main(int argc, char *argv[])
 {
-	if (argc < 2)
+	if (argc != 3)
 	{
 		printf("Eroro\n");
 		return (1);
 	}
 
-	printf("%d\n", argv[0] * argv[1]);
+	printf("%d\n", argv[1] * argv[2]);
 
 	return (0);
 }
