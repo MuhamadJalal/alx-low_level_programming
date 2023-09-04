4057206376881942bf5fea496d0d62cb0d0d6089
MuhamadJalal <muhamadgalal4@gmail.com>
2023-09-04 15:53:57 -0700

    Add 2-str_concat.c file

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -2,8 +2,49 @@
 #include <stdlib.h>
 #include "main.h"
 
-int _strlen(char *s);
-char *_strncat(char *dest, char *src, int n);
+/**
+* _strncat - concatenate src to dest string.
+* @dest: pointer points to the string that will be appended to.
+* @src: pointer points to the string needs to add to dest
+* @n: the size of src string.
+* Return: pointer to char
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+	int dest_len = 0, src_len = 0;
+
+	for (dest_len = 0; dest[dest_len] != '\0'; dest_len++)
+	;
+
+	for (src_len = 0; src_len < n && src[src_len] != '\0'; src_len++)
+	{
+		dest[dest_len] = src[src_len];
+		dest_len++;
+	}
+	if (src_len < n)
+	{
+		dest[dest_len] = '\0';
+	}
+	return (dest);
+}
+
+/**
+* _strlen - entry point
+* the pointer points to char needs to know its length
+* @s: pointer to the variable to update
+* Return: int
+*/
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
+}
 
 /**
 * str_concat - concatenates two strings
