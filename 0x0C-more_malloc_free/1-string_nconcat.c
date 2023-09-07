89ed6be50d0bc7e6119cbb004957daea6f8740f2
MuhamadJalal <muhamadgalal4@gmail.com>
2023-09-07 15:26:06 -0700

    Add 1-string_nconcat.c file

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,9 +17,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	int size_s1, size_s2;
 	int i, j = 0;
 
-	if (s1 == '\0' && s2 == '\0')
-		return ('\0');
-
 	while (s1 && s1[size_s1] != '\0')
 		size_s1++;
 
