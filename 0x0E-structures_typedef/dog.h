8a654b7562caf6c9cbe2b5409d5878125c474a97
MuhamadJalal <muhamadgalal4@gmail.com>
2023-09-11 15:48:33 -0700

    Add 4-new_dog.c file

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -13,7 +13,9 @@ typedef struct dog
 	char *name;
 	float age;
 	char *owner;
-} dog_t;
+};
+
+typedef dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 dog_t *new_dog(char *name, float age, char *owner);
