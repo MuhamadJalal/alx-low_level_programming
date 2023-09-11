97441a215f55a685613b6a7de6057694ac490a0a
MuhamadJalal <muhamadgalal4@gmail.com>
2023-09-10 20:51:17 -0700

    Add dog.h file

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - structered data
+*
+* @name: the dog name
+* @age: the dog age
+* @owner: the owner of the dog
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
