f6954815503832034d49fbcb54b580102007ec90
MuhamadJalal <muhamadgalal4@gmail.com>
2023-09-10 22:58:05 -0700

    Add 2-print_dog.c file

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,30 @@
+#incude "dog.h"
+
+/**
+* print_dog - prints a struct dog
+*
+* @d: the pointer struct of type dog
+*
+* Return: void
+*/
+
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		printf("Name: %p", (void *) d->name);
+	else
+		printf("Name: %s", d->name);
+
+	if (d->age == NULL)
+		printf("Age: %p", (void *) d->age);
+	else
+		printf("age: %d", d->age);
+
+	if (d->owner == NULL)
+		printf("Owner: %p", (void *) d->owner);
+	else
+		printf("Owner: %s", d->owner);
+}
