5b9a83c22192d738031dc8d5304af5b7e16c66c0
MuhamadJalal <muhamadgalal4@gmail.com>
2023-09-13 13:04:45 -0700

    Add setup file

---
+++ 0x0F-function_pointers/3-calc.h
@@ -0,0 +1,12 @@
+#ifndef function_pointers
+#define function_pointers
+
+int _putchar(char c);
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+int (*get_op_func(char *s))(int, int);
+
+#endif
