79a45c0edd1e42a8d196459183ef8774c1211d17
MuhamadJalal <muhamadgalal4@gmail.com>
2023-09-13 16:28:53 -0700

    Add operators functions files

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,11 +1,9 @@
 #ifndef CALC_H
 #define CALC_H
-
 /**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
+ * struct op - A struct op.
+ * @op: The operator.
+ * @f: The associated function.
  */
 typedef struct op
 {
@@ -13,7 +11,6 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
-int _putchar(char c);
 int op_add(int a, int b);
 int op_sub(int a, int b);
 int op_mul(int a, int b);
