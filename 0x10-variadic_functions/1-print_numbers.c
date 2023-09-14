fef6646dc7b5bd2f281e8334b544495a96843508
MuhamadJalal <muhamadgalal4@gmail.com>
2023-09-14 15:07:25 -0700

    Add 1-print_numbers.c file

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	{
 		va_start(vl, n);
 
-		for (i = 0; i < n; i++)
+		for (i = 1; i < n; i++)
 		{
 			printf("%d", va_arg(vl, int));
 
