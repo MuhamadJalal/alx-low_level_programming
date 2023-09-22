df2b3b3263826e1152b8a163f0a03f5928e48618
MuhamadJalal <muhamadgalal4@gmail.com>
2023-09-22 11:21:14 -0700

    Add 2-add_node.c file

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -14,21 +14,14 @@ list_t *add_node(list_t **head, const char *str)
 	if (new_list_t != NULL)
 		return (NULL);
 
-	new_list_t->str = strdup(str);
-
-	if (new_list_t->str == NULL)
-	{
-		free(new_list_t);
-		return (NULL);
-	}
-
 	while (str[len])
 		len++;
 
-	new_list_t->next = *head;
+	new_list_t->str = strdup(str);
 	new_list_t->len = len;
+	new_list_t->next = (*head);
 
-	*head = new_list_t;
+	(*head) = new_list_t;
 
 	return (new_list_t);
 }
