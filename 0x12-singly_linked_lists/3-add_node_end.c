030b1737b6e020b590a421bc2912de9063df72ed
MuhamadJalal <muhamadgalal4@gmail.com>
2023-09-22 12:43:07 -0700

    Add 3-add_node_end.c file

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,38 +1,38 @@
 #include "lists.h"
 
 /**
- * *add_node - adds a new node at the beginning of a list_t list
+ * *add_node_end - adds a new node at the beginning of a list_t list
  * @str: needs to be duplicated
  * @head: a struct input
  * Return: the address of the new element
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *new_list_t;
+	list_t *new;
 	list_t *temp = *head;
 	unsigned int len = 0;
 
 	while (str[len])
 		len++;
 
-	new_list_t = malloc(sizeof(list_t));
-	if (!new_list_t)
+	new = malloc(sizeof(list_t));
+	if (!new)
 		return (NULL);
 
-	new_list_t->str = strdup(str);
-	new_list_t->len = len;
-	new_list_t->next = NULL;
+	new->str = strdup(str);
+	new->len = len;
+	new->next = NULL;
 
 	if (*head == NULL)
 	{
-		*head = new_list_t;
-		return (new_list_t);
+		*head = new;
+		return (new);
 	}
 
 	while (temp->next)
 		temp = temp->next;
 
-	temp->next = new_list_t;
+	temp->next = new;
 
-	return (list_len);
+	return (new);
 }
