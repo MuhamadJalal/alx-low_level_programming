ff8c92603f4bce5bf89fc1c380707b00ee6ff6f5
MuhamadJalal <muhamadgalal4@gmail.com>
2023-09-22 19:51:10 -0700

    Add 100-first.c file

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+/**
+* first - function that prints "text"
+* before the main function is executed
+*/
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
