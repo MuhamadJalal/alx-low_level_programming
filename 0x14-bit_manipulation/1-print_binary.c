e029fcd86c0bc08a00e918cd5063d3b41aef8cfe
Muhamad Jalal <33559652+MuhamadJalal@users.noreply.github.com>
2023-09-29 02:51:14 +0300

    Add 1-print_binary.c file

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,8 +8,14 @@
  */
 void print_binary(unsigned long int n)
 {
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
+
 	if (n > 1)
 		print_binary(n >> 1);
 
-	printf("%lu", n % 2);
+	_putchar(48 + (n % 2));
 }
