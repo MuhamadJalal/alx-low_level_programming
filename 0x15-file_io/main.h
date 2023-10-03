52bc255be81d1b082466cfe230eb042e82e543ea
Muhamad Jalal <33559652+MuhamadJalal@users.noreply.github.com>
2023-10-04 00:36:42 +0300

    add all files once!

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -15,17 +15,15 @@ int append_text_to_file(const char *filename, char *text_content);
 char *buffer_generator(char *filename);
 void close_file(int file_descriptor);
 
-int _strncmp(const char *s1, const char *s2, size_t n);
-void _close(int fd);
-void _read(int fd, char *buf, size_t count);
-void elf_magic(const unsigned char *buffer);
-size_t elf_class(const unsigned char *buffer);
-int elf_data(const unsigned char *buffer);
-void elf_version(const unsigned char *buffer);
-void elf_osabi(const unsigned char *buffer);
-void elf_abivers(const unsigned char *buffer);
-void elf_type(const unsigned char *buffer, int big_endian);
-void elf_entry(const unsigned char *buffer, size_t bit_mode, int big_endian);
-
+void check_elf(unsigned char *e_ident);
+void print_magic(unsigned char *e_ident);
+void print_class(unsigned char *e_ident);
+void print_data(unsigned char *e_ident);
+void print_version(unsigned char *e_ident);
+void print_abi(unsigned char *e_ident);
+void print_osabi(unsigned char *e_ident);
+void print_type(unsigned int e_type, unsigned char *e_ident);
+void print_entry(unsigned long int e_entry, unsigned char *e_ident);
+void close_elf(int elf);
 
 #endif
